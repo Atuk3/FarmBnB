7196df02ea2ab360b39ee33cbaaa968381e17b9a
David Owamagbe <david.owamagbe@students.plymouth.ac.uk>
2021-11-01 17:49:20 +0000

    update

--- app/src/main/java/com/example/farmbnb/MainActivity.java
+++ app/src/main/java/com/example/farmbnb/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.farmbnb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean checkIfFieldsAreCorrect()
+    {
+        EditText pnme= findViewById(R.id.PersonName);
+        String pname= ((EditText)findViewById(R.id.PersonName)).getText().toString();
+        EditText psd= findViewById(R.id.PersonPassword);
+        String passwd= ((EditText)findViewById(R.id.PersonPassword)).getText().toString();
+        EditText agee= findViewById(R.id.age);
+        String personAge= ((EditText)findViewById(R.id.age)).getText().toString();
+        int pAge=Integer.parseInt(personAge);
+
+        if(!pname.equalsIgnoreCase("admin"))
+        {
+
+            Snackbar.make(pnme,"Username field is incorrect", Snackbar.LENGTH_SHORT)
+                    .show();
+            return false;
+        }
+
+        if(!passwd.equals("fx65dg."))
+        {
+
+            Snackbar.make(psd,"Password field is incorrect", Snackbar.LENGTH_SHORT)
+                    .show();
+            return false;
+        }
+        if(pAge<18)
+        {
+
+            Snackbar.make(agee,"You have to be 18 or older", Snackbar.LENGTH_SHORT)
+                    .show();
+            return false;
+        }
+
+
+       return true;
+
+
+    }
+
+    public void validate(View signup)
+    {
+        if(checkIfFieldsAreCorrect())
+        {
+
+            Intent intent= new Intent(this, Explore.class);
+            startActivity(intent);
+        }
+
+
+
+
     }
 }
